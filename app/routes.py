acf86f37101ec18f6c8307446f19810e29382a43
technopig <sjcip@umich.edu>
2021-05-04 13:19:01 -0600

    added a delay for demonstration purposes

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,18 @@ from flask import render_template, request
 from weather import get_weather
 from water_gauge import get_cfs_tomorrow
 from avy import get_avy_frontrange
+import logging
+import json_log_formatter
+
+formatter = json_log_formatter.JSONFormatter()
+
+json_handler = logging.FileHandler(filename='/Users/sam.cipriani/github/activity_picker/activity_picker.log')
+json_handler.setFormatter(formatter)
+
+logger = logging.getLogger('my_json')
+logger.addHandler(json_handler)
+logger.setLevel(logging.INFO)
+
 
 @app.route('/')
 @app.route('/index')
@@ -13,12 +25,16 @@ def index():
     forecasts = []
     # boulder_forecast =
     forecasts.append(get_weather("40.0338,-105.2561", "Boulder"))
+    logger.info('Got weather', extra={'location': 'Boulder', 'value': forecasts[len(forecasts)-1]})
     # eldora_forecast =
     forecasts.append(get_weather("39.93646991379455,-105.58600635354678", "Eldora"))
+    logger.info('Got weather', extra={'location': 'Eldora', 'value': forecasts[len(forecasts)-1]})
     # water_info =
     forecasts.append(get_cfs_tomorrow("06730200", "Boulder Creek"))
+    logger.info('Got water info', extra={'location': 'Boulder Creek', 'value': forecasts[len(forecasts)-1]})
     # avy_info =
     forecasts.append(get_avy_frontrange())
+    logger.info('Got Avy info', extra={'location': 'Front Range', 'value': forecasts[len(forecasts)-1]})
     return render_template('index.html', user=user, forecasts=forecasts)
 
 #
